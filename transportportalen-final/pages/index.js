62cb5756db26ab51ab66292e0c670cbd1848abfe
tjansteportalen <tjansteportalen@hotmail.com>
2025-04-06 01:37:55 +0200

    Add files via upload

---
+++ transportportalen-final/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Transportportalen</h1>
+      <p>Välkommen! Din hemsida fungerar nu.</p>
+    </div>
+  );
+}
